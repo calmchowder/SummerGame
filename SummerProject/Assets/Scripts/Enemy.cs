eaacaa790219b826bbcb315f55a30c8eb3a6d4d8
jeremysu1 <jeremycksu@yahoo.com>
2018-06-01 20:48:12 -0400

    Added Jeremy_Test scene, general enemy script and RangedEneemy script

---
+++ SummerProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField]
+    public Transform target;
+    protected NavMeshAgent ThisAgent = null;
+
+    public float health;
+    public List<Bullet> bulletList;
+    public Rigidbody rb;
+    //public Collider3D collider;
+
+    public void Attack()
+    {
+        
+    }
+
+    public void Move()
+    {
+
+    }
+}
---
+++ SummerProject/Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class RangedEnemy : Enemy {
+
+    [SerializeField]
+    private float firingCD = 1.0f;
+
+    void Start()
+    {
+        health = 40;
+        ThisAgent = GetComponent<NavMeshAgent>();
+    }
+
+	new void Attack () {
+		
+	}
+	
+
+	new void Move () {
+        ThisAgent.SetDestination(target.position);
+	}
+
+    bool InRange()
+    {
+        return false;
+    }
+
+    bool CanSee()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = origin - target.position;
+        return Physics.Raycast(origin, direction.normalized, Mathf.Infinity, 3);
+    }
+
+    void Update()
+    {
+        if (CanSee())
+            Debug.Log("Can see the player");
+        Move();
+    }
+}
